eda03b6320634a1b8a5a4702a8ab11411dd0a76a
TegarMachfuzi <tegarmachfudzi99@gmail.com>
2024-06-05 23:47:46 +0700

    test

--- src/main/java/com/example/productservice/repository/ProductRepository.java
+++ src/main/java/com/example/productservice/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.example.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository  extends MongoRepository<Product, String> {
+
 }
